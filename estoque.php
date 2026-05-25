bc5cb316b583c24eeb8fc23c4107464dd0c5bf08
Livspl28 <livstml28@gmail.com>
2026-05-25 16:53:52 -0300

    Update estoque.php

--- estoque.php
+++ estoque.php
@@ -1,181 +1,133 @@
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Controle de estoque</title>
-    <link rel="stylesheet" href="./css/estoque.css">
-     <link rel="stylesheet" href="./css/global.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"/>
-</head>
-<body>
-
 <?php
- include 'partials/sidebar.php'
+
+
+
+    $titulo = 'Byteware';
+    $css = './css/estoque.css';
+    require_once 'crud.php';
+    require_once 'partials/sidebar.php';  
+
+
+    if (isset($_GET['erro'])){
+        $erro = $_GET['erro'];
+        if ($erro === 'erro_linkinvalido'){	
+        echo '<h1 class="msg_erro">Erro: Esse link que você tentou acessar é invalido.</h1>' ;
+        }
+        else{
+        echo '<h1 class="msg_erro"> Um erro desconhecido ocorreu</h1>';
+        }	
+    }	
+
 ?>
 
+    <body>
+
 <div class="container">
+    
  <div class="card-box produtos-box">
+    <i class="bi bi-boxes"></i>
     <h4>Total</h4>
  </div>
 
  
  <div class="card-box produtosbaixos-box">
+    <i class="bi bi-exclamation-circle"></i>
      <h4>Perto do mínimo</h4>
  </div>
 
 
  
 <div class="card-box produtosabaixo-box">
+       <i class="bi bi-exclamation-triangle"></i>
      <h4>Abaixo do Mínimo</h4>
  </div>
 
 
  
  <div class="card-box valor-box">
+    <i class="bi bi-currency-dollar"></i>
      <h4>Valor total</h4>
  </div>
  
 </div>
 
-<div class="box-filtro"></div>
 
-            <section class="box-tabela">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Nome</th>
-                                <th>PN</th>
-                                <th>Estoque</th>
-                                <th>Status</th>
-                                <th>Categoria</th>
-                                <th>Custo</th>   <th>Ajustes</th>
-                            </tr>
-                        </thead>
-                        <tbody> 
-                            <tr>
-                                <td>133</td>
-                                <td>Voce</td>
-                                <td>3523</td>
-                                <td>6757</td>
-                                <td>ativp</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td><i class="bi bi-eye"></i></td>
-                            </tr>
+            <main class="box-tabela">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>PN</th>
+                            <th>Estoque</th>
+                            <th>Categoria</th>
+			                <th>Custo</th>   
+			                <th>Descrição</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                        require_once 'crud.php';
+                        $produtos = readAll($pdo, 'produtos');
+                        $conteudo_tabela = null;
+                        foreach($produtos as $produto){
+                            if ($produto['estoque'] > 50 ){
+                                $status = 'estoque_padrao';
+                            }
+                            elseif ($produto['estoque'] <= 50 && $produto['estoque'] > 10){
+                                $status = 'estoque_baixo';
+                            }
+                            elseif ($produto['estoque'] <= 10 && $produto['estoque'] > 0){
+                                $status = 'estoque_mtbaixo';
+                            }
+                            elseif($produto['estoque'] <= 0){
+                                $dados_atualizados = [
+                                'estoque' => 0,
+                                'ativo' => 0
+				];
+                                $status_db = update($pdo, 'produtos', $dados_atualizados, 'id_produto='.$produto['id_produto'].''); 
+                                $status = 'estoque_zerado';
+			    }
 
-                            <tr>
-                                <td>133</td>
-                                <td>hnhghn</td>
-                                <td>3523</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td><i class="bi bi-eye"></i></td>
-                            </tr>
-                            
-                            <tr>
-                                <td>133</td>
-                                <td>hnhghn</td>
-                                <td>3523</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td><i class="bi bi-eye"></i></td>
-                            </tr>
+			    if ($produto['custo'] < 0){
+				    $dados_atualizados = 
+					    [
+						'custo' => 0
+					    ];	
+                                $status_db = update($pdo, 'produtos', $dados_atualizados, 'id_produto='.$produto['id_produto'].''); 
+			  }
+                            $atividade = null;
+                            if ($produto['ativo'] == TRUE){
+                                $atividade = 'Ativo';                            
+                                }
+                            else{
+                                $atividade = 'Inativo';
+                            }
+                            $conteudo_tabela .= '<tr class='.$status.'><td>'.$produto['id_produto'].'</td><td>'.$produto['nome_produto'].'</td><td>'.$produto['pn'].'</td><td>'.$produto['estoque'].'</td><td>'.$atividade.'</td><td>'.$produto['categoria'].'</td><td>'.$produto['custo'].'</td><td><form action="descricaoPro.php" method="GET"><button value='.$produto['id_produto'].' name="p_editar"><i class="bi bi-eye"></i></button></form></td>';
+                        } 
+                        echo $conteudo_tabela;
+                    ?>
+                    </tbody>
+                </table>
+</main>
 
-                            <tr>
-                                <td>133</td>
-                                <td>hnhghn</td>
-                                <td>3523</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td><i class="bi bi-eye"></i></td>
-                            <tr>
 
-                                <td>133</td>
-                                <td>hnhghn</td>
-                                <td>3523</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                            </tr>
-
-                            <tr>
-                                <td>133</td>
-                                <td>hnhghn</td>
-                                <td>3523</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                            </tr>
-
-                            <tr>
-                                <td>133</td>
-                                <td>hnhghn</td>
-                                <td>3523</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                            </tr>
-
-                            <tr>
-                                <td>133</td>
-                                <td>hnhghn</td>
-                                <td>3523</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                                <td>hnhghn</td>
-                            </tr>
-
-                        <!-- 
-                            // require_once 'crud.php';
-                            // $produtos = readAll($pdo, 'produtos');
-                            // $conteudo_tabela = null;
-                            // foreach($produtos as $produto){
-                                // if ($produto['estoque'] > 50 ){
-                                    // $status = '';
-                                // }
-                                // elseif ($produto['estoque'] <= 50 && $produto['estoque'] > 10){
-                                    // $status = 'estoque_baixo';
-                                // }
-                                // elseif ($produto['estoque'] <= 10 && $produto['estoque'] > 0){
-                                    // $status = 'estoque_mtbaixo';
-                                // }
-                                // elseif($produto['estoque'] <= 0){
-                                    // $dados_atualizados = [
-                                    // 'estoque' => 0,
-                                    // 'ativo' => FALSE
-                                    // ];
-                                    // $status_db = update($pdo, 'produtos', $dados_atualizados, 'id_produto='.$produto['id_produto'].''); 
-                                    // $status = 'estoque_zerado';
-                                // }
-                                // $atividade = null;
-                                // if ($produto['ativo'] == TRUE){
-                                    // $atividade = 'Ativo';                            
-                                    // }
-                                // else{
-                                    // $atividade = 'Inativo';
-                                // }
-                                // $conteudo_tabela .= '<tr class='.$status.'><td>'.$produto['id_produto'].'</td><td>'.$produto['nome_produto'].'</td><td>'.$produto['pn'].'</td><td>'.$produto['estoque'].'</td><td>'.$atividade.'</td><td>'.$produto['categoria'].'</td><td>'.$produto['custo'].'</td><td><a href=descricaoPro.php><i class="bi bi-eye"></i></a></td>';
-                            // } 
-                            // echo $conteudo_tabela;
-                        ?> -->
-                        </tbody>
+        <section class="box-tabela">
+           <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>PN</th>
+                            <th>Estoque</th>
+                            <th>Status</th>
+                            <th>Categoria</th>
+			                <th>Custo</th>   
+			                <th>Descrição</th>
+                        </tr>
+                    </thead>
                     </table>
-            </section>
+</section>
     </body>
 </html>
