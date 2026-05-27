5eabc6c0ba5f193399c499dbeca5c1e80364d6f5
Livspl28 <livstml28@gmail.com>
2026-05-27 15:06:57 -0300

    Update validacaologin.php

--- validacaologin.php
+++ validacaologin.php
@@ -1,7 +1,7 @@
 <?php
-// Quando a pessoa logar ela vai ser direcionada para esse arquivo levando GET=1
 
 $clientes = readAll($pdo, 'clientes');
+$administradores = readAll($pdo, 'administradores');
 
 // Aqui vai pegar os dados do formulário de login
     $verificarUsuario = [
@@ -9,8 +9,17 @@ $clientes = readAll($pdo, 'clientes');
     'senha' => $_POST['senha-login']
     ];
 
+session_unset();
 
-
+// Aqui vai ser a verificação se é adm ou não
+foreach($administradores as $mostrarAdm){
+        if($mostrarAdm['email'] == $verificarUsuario['email'] && $mostrarAdm['senha'] == $verificarUsuario['senha']){
+            $_SESSION['admLogado'] = [];
+            $_SESSION['admLogado'] = $mostrarAdm;
+            header('Location: estoque.php');
+            exit;
+        }
+}
 
 // Aqui vai verificar todo o banco de dados e procurar se tem alguém com o mesmo email e senha que acabou de ser digitado no form
     foreach($clientes as $mostrarCliente){
@@ -18,12 +27,11 @@ $clientes = readAll($pdo, 'clientes');
             // Agora que achou tem que abrir o session
             $_SESSION['clienteLogado'] = [];
             $_SESSION['clienteLogado'] =  $mostrarCliente;
-            header('Location: select.php');
+            header('Location: index.php');
             exit;
         }
     }
 
 // Se ele chegar nessa parte é por que o foreach rodou inteiro e não achou a pessoa certa
 header('Location: form_login.php?erro=erro_login');
-
 ?>
