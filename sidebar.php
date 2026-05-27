26bc0c11c5431cf6c7d2d2af2a9d6f4be08ee2dc
Livspl28 <livstml28@gmail.com>
2026-05-27 15:06:35 -0300

    Refactor sidebar structure and update links

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,22 @@
-<header class="side">
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $titulo ?></title>
+        <link rel="stylesheet" href="./css/global.css">
+        <link rel="stylesheet" href="<?php echo $css?>">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"/>
+    </head>
+
+        <header class="side">
             <div class="col">
-                <a href="vendas.php"><img src="./imagens/logo.png" alt="" class="logo"/></a>
-                <a href="vendas.php" class="nome">ByteWare</a>
+                <a href="index.php"><img src="./imagens/logo.png" alt="" class="logo"/></a>
+                <a href="index.php" class="nome">ByteWare</a>
             </div>
         <nav class="ancoras">
-            <a href="index.php" class="<?= $pagina == 'estoque' ? 'active' : '' ?>"><i class="bi bi-table"></i>Painel de Estoque</a>
-            <a href="cadastro.php" class="<?= $pagina == 'cadastrar' ? 'active' : '' ?>"><i class="bi bi-clipboard-plus-fill"></i>Cadastrar Produto</a>
+            <a href="estoque.php" class="<?= $pagina == 'estoque' ? 'active' : '' ?>"><i class="bi bi-table"></i>Painel de Estoque</a>
+            <a href="cadastroPro.php" class="<?= $pagina == 'cadastro' ? 'active' : '' ?>"><i class="bi bi-clipboard-plus-fill"></i>Cadastrar Produto</a>
             <a href="financeiro.php" class="<?= $pagina == 'financeiro' ? 'active' : '' ?>"><i class="bi bi-bar-chart-line-fill"></i>Visão Geral</a>
         </nav>
         </header>
