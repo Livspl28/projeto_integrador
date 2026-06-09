e962a8b06b39fe122ba9db72be06e990f809858c
Livspl28 <livstml28@gmail.com>
2026-06-09 16:42:25 -0300

    Update estoque.php

--- estoque.php
+++ estoque.php
@@ -12,41 +12,117 @@
         } else {
             echo '<h1 class="msg_erro">Um erro desconhecido ocorreu</h1>';
         }   
-    }   
+    }  
+    $estoque_total = 0;
+    $estoque_baixoTotal = 0;
+    $estoque_minTotal = 0;
+    $estoque_zero =  0;
+    $valor_total = 0;
+
+
+    $produtos = readAll($pdo, 'produtos');
+    $conteudo_tabela = null;
+    $conteudo_tabela_zerada = null;
+
+    foreach($produtos as $produto){
+    $zerado = false;
+
+
+        if ($produto['preco'] > 0 ){
+            $valor_total = $valor_total + $produto['preco'] * $produto['estoque'];
+        }
+
+
+        if ($produto['estoque'] > 0 ){
+            $estoque_total = $estoque_total + $produto['estoque'];
+        }
+
+
+
+
+        if ($produto['estoque'] <= 250 && $produto['estoque'] > 100){
+            $estoque_baixoTotal = $estoque_baixoTotal + 1;
+        }
+        elseif ($produto['estoque'] <= 100 && $produto['estoque'] > 0){
+            $estoque_minTotal = $estoque_minTotal + 1;
+        }
+        elseif ($produto['estoque'] <= 0 ){
+            $estoque_zero = $estoque_zero + 1;
+        }
+    
+
+    }
+
+
 ?>
 
 <body>
     <main>
         <div class="container">
-            <div class="card-box produtos-box">
-                <i class="bi bi-boxes"></i>
-                <h4>Total</h4>
-            </div>
+            <a href="#produtos-geral" class="card-link">
+                <div class="card-box produtos-box">
+                    <div class="quantidade">
+                        <i class="bi bi-boxes"></i>
+                        <h4 class="number"><?php print number_format($estoque_total, 0, ',', '.');?></h4>
+                    </div>
+                    <div class="ali">
+                        <h4>Total</h4>
+                    </div>
+                </div>
+            </a>
 
-            <div class="card-box produtosbaixos-box">
-                <i class="bi bi-exclamation-circle"></i>
-                <h4>Perto do mínimo</h4>
-            </div>
+            <a href="#produtos-geral" class="card-link">
+                <div class="card-box produtosbaixos-box">
+                    <div class="quantidade">
+                        <i class="bi bi-exclamation-circle"></i>
+                        <h4 class="number"><?php print $estoque_baixoTotal;?></h4>
+                    </div>
+                    <div class="ali">
+                        <h4>Perto do mínimo</h4>
+                    </div>
+                </div>
+            </a>
 
-            <div class="card-box produtosabaixo-box">
-                <i class="bi bi-exclamation-triangle"></i>
-                <h4>Abaixo do Mínimo</h4>
-            </div>
+            <a href="#produtos-geral" class="card-link">
+                <div class="card-box produtosabaixo-box">
+                    <div class="quantidade">
+                        <i class="bi bi-exclamation-triangle"></i>
+                        <h4 class="number"><?php print $estoque_minTotal;?></h4>
+                    </div>
+                    <div class="ali">
+                        <h4>Abaixo do Mínimo</h4>
+                    </div>
+                </div>
+            </a>
 
-            <div class="card-box valor-box">
-                <i class="bi bi-currency-dollar"></i>
-                <h4>Valor total</h4>
-            </div>
+            <a href="#produtos-geral" class="card-link">
+                <div class="card-box valor-box">
+                    <div class="quantidade">
+                        <i class="bi bi-currency-dollar"></i>
+                        <h4 class="number">R$<?php print number_format($valor_total, 2, ',', '.');?></h4>
+                    </div>
+                    <div class="ali">
+                        <h4>Valor total</h4>
+                    </div>
+                </div>
+            </a>
 
-            <div class="card-box produtos-inativos">
-                <i class="bi bi-ban"></i>
-                <h4>Inativos</h4>
-            </div>
+            <a href="#produtos-sem-estoque" class="card-link">
+                <div class="card-box produtos-inativos">
+                    <div class="quantidade">
+                        <i class="bi bi-ban"></i>
+                        <h4 class="number"><?php print $estoque_zero;?></h4>
+                    </div>
+                    <div class="ali">
+                        <h4>Inativos</h4>
+                    </div>
+                </div>
+            </a>
         </div>
 
 
 
-        <h1>Produtos geral</h1>
+        <h1 id="produtos-geral">Produtos geral</h1>
         <section class="box-tabela">
             <table class="teste">
                 <thead>
@@ -69,14 +145,16 @@
             
                         foreach($produtos as $produto){
                             $zerado = false;
+
+                            
             
-                            if ($produto['estoque'] > 50 ){
+                            if ($produto['estoque'] > 250 ){
                                 $status = 'estoque_padrao';
                             }
-                            elseif ($produto['estoque'] <= 50 && $produto['estoque'] > 10){
+                            elseif ($produto['estoque'] <= 250 && $produto['estoque'] > 100){
                                 $status = 'estoque_baixo';
                             }
-                            elseif ($produto['estoque'] <= 10 && $produto['estoque'] > 0){
+                            elseif ($produto['estoque'] <= 100 && $produto['estoque'] > 0){
                                 $status = 'estoque_mtbaixo';
                             }
                             elseif ($produto['estoque'] <= 0){
@@ -95,9 +173,9 @@
                             $atividade = ($produto['status'] == true) ? 'Ativo' : 'Inativo';
                             $linha_html = '<tr class="'.$status.'">
                                 <td><img src="'.$produto['imagem'].'" width="80" alt="Imagem"></td>
-                                <td>'.$produto['nome_produto'].'</td>
+                                <td class="status-nome">'.$produto['nome_produto'].'</td>
                                 <td>'.$produto['pn'].'</td>
-                                <td>'.$produto['estoque'].'</td>
+                                <td><p class="status">'.$produto['estoque'].'</p></td>
                                 <td>'.$atividade.'</td>
                                 <td>'.$produto['categoria'].'</td>
                                 <td>R$ '.number_format($produto['preco'], 2, ',', '.').'</td>
@@ -120,7 +198,7 @@
         </section>
 
 
-        <h1>Produtos sem estoque</h1>
+        <h1 id="produtos-sem-estoque">Produtos sem estoque</h1>
         <section class="box-tabela">
             <table>
                 <thead>
