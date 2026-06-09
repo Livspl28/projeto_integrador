9825976cfeb07355ed654f84f6b95477c8169bbb
Livspl28 <livstml28@gmail.com>
2026-06-09 16:43:11 -0300

    Update form_login.php

--- form_login.php
+++ form_login.php
@@ -13,7 +13,7 @@ $login_invalido = $_GET['erro'] ?? null;
 <body>
 
     <div class="container">
-        <form action="init.php" method="post" class="log">
+        <form action="process/init.php" method="post" class="log">
 
             <h1><i class="bi bi-person"></i>LOGIN</h1>
 
@@ -22,24 +22,24 @@ $login_invalido = $_GET['erro'] ?? null;
                 <label for="email">E-mail</label>
             </div>
 
-            <input type="text" placeholder="Insira o seu e-mail" id="email" name="email-login" class="inp">
+            <input type="text" placeholder="Insira o seu e-mail" id="email" maxlength="500" name="email-login" class="inp">
 
             <div class="rotulo">
                 <i class="bi bi-lock"></i>
                 <label for="senha">Senha</label>
             </div>
-            <input type="text" placeholder="Insira a sua senha" id="senha" name="senha-login" class="inp">
+            <input type="password" placeholder="Insira a sua senha" id="senha" name="senha-login" maxlength="1000" class="inp" autocomplete="current-password">
 
             <button type="submit" class="btn">Login</button>
             <?php
-            if($login_invalido == 'erro_login'){
+            if ($login_invalido === 'erro_login') {
                 echo '<h3>E-mail ou senha inválidos</h3>';
+            } elseif ($login_invalido === 'restrito') {
+                echo '<h3>Acesso restrito. Faça login primeiro.</h3>';
             }
-            elseif($login_invalido =='restrito');
-            ?>
-
+            ?> 
             <div class="redirecionar">
-                <p>Ainda não se cadastrou? <a href="form_cadastro.php" class="cdt">Acesse aqui</a> </p>
+                <p>Ainda não se cadastrou? <a href="form_cadastro.php" class="cdt">Acesse aqui</a></p>
             </div>
         </form>
     </div>
